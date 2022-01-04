24f7778da442b7e5f46c48c9b2a320fc06a80393
Vishwajeet Yadav <yadavvishwajeet880@gmail.com>
2022-01-05 03:20:28 +0530

    Added DB Params Constants

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'root',
-    DB_NAME: 'vidly_movies'
+    HOST: 'us-cdbr-east-05.cleardb.net',
+    USER: 'b3bf0c184a1c8b',
+    PASSWORD: 'cf209dc2',
+    DB_NAME: 'heroku_97a33d8fa0d4d08'
 }
