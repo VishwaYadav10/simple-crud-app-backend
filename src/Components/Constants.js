a913224d81f656557ae6500f95802edba56c0b49
Vishwajeet Yadav <yadavvishwajeet880@gmail.com>
2022-01-05 03:38:41 +0530

    Creds changed in constants

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: 'us-cdbr-east-05.cleardb.net',
-    USER: 'b3bf0c184a1c8b',
-    PASSWORD: 'cf209dc2',
-    DB_NAME: 'heroku_97a33d8fa0d4d08'
+    USER: 'bf1dde526230fa',
+    PASSWORD: 'e107504d',
+    DB_NAME: 'heroku_990d2024132d13a'
 }
